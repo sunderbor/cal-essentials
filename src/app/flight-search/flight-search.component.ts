2c759ee15add119b75d677387902e6f271491166
Alex Thalhammer <alex@angular-software.at>
2021-06-30 08:20:25 +0200

    Bonus: Edit flights

--- src/app/flight-search/flight-search.component.ts
+++ src/app/flight-search/flight-search.component.ts
@@ -14,28 +14,45 @@ export class FlightSearchComponent implements OnInit {
   flights: Flight[] = [];
   selectedFlight: Flight;
 
+  message: string;
+
+  private url = 'http://www.angular.at/api/flight';
+  private headers = new HttpHeaders().set('Accept', 'application/json');
+
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit(): void {}
 
   search(): void {
-    let url = 'http://www.angular.at/api/flight';
+    const params = new HttpParams().set('from', this.from).set('to', this.to);
 
-    let headers = new HttpHeaders().set('Accept', 'application/json');
-
-    let params = new HttpParams().set('from', this.from).set('to', this.to);
-
-    this.http.get<Flight[]>(url, { headers, params }).subscribe({
+    this.http.get<Flight[]>(this.url, { headers: this.headers, params }).subscribe({
       next: (flights: Flight[]) => {
         this.flights = flights;
       },
       error: (errResp) => {
         console.error('Error loading flights', errResp);
+      },
+      complete: () => {
+        console.warn('complete');
       }
     });
   }
 
   select(f: Flight): void {
     this.selectedFlight = f;
+  }
+
+  save(): void {
+    this.http.post<Flight>(this.url, this.selectedFlight, { headers: this.headers }).subscribe({
+      next: (flight) => {
+        this.selectedFlight = flight;
+        this.message = 'Success!';
+      },
+      error: (errResponse) => {
+        console.error('Error', errResponse);
+        this.message = 'Error: ';
+      }
+    });
   }
 }
