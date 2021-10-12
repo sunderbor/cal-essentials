8209484adb887d3a527706e00c03159634a3a4af
Alex Thalhammer <alex@angular-software.at>
2021-10-12 14:51:02 +0200

    [T-d forms] Own validator

--- src/app/flight-booking/flight-booking.module.ts
+++ src/app/flight-booking/flight-booking.module.ts
@@ -6,10 +6,17 @@ import { FlightSearchComponent } from './flight-search/flight-search.component';
 import { FlightCardComponent } from './flight-card/flight-card.component';
 import { FlightStatusToggleComponent } from './flight-status-toggle/flight-status-toggle.component';
 import { FlightValidationErrorsComponent } from './flight-validation-errors/flight-validation-errors.component';
+import { CityValidatorDirective } from './shared/validation/city-validator.directive';
 
 @NgModule({
   imports: [CommonModule, SharedModule],
-  declarations: [FlightSearchComponent, FlightCardComponent, FlightStatusToggleComponent, FlightValidationErrorsComponent],
+  declarations: [
+    FlightSearchComponent,
+    FlightCardComponent,
+    FlightStatusToggleComponent,
+    FlightValidationErrorsComponent,
+    CityValidatorDirective
+  ],
   providers: [],
   exports: [FlightSearchComponent]
 })
