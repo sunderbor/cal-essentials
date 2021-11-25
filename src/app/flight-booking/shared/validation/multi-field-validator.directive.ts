76e556db2af1187a62ee5094fef6349695cb26cd
Alex Thalhammer <alex@angular-software.at>
2021-11-25 13:44:53 +0100

    [T-d forms] Bonus: Parametrizable Multifield Validator

--- src/app/flight-booking/shared/validation/multi-field-validator.directive.ts
+++ src/app/flight-booking/shared/validation/multi-field-validator.directive.ts
@@ -1,4 +1,4 @@
-import { Directive } from '@angular/core';
+import { Directive, Input } from '@angular/core';
 import { AbstractControl, FormGroup, NG_VALIDATORS, ValidationErrors, Validator } from '@angular/forms';
 
 @Directive({
@@ -6,11 +6,17 @@ import { AbstractControl, FormGroup, NG_VALIDATORS, ValidationErrors, Validator 
   providers: [{ provide: NG_VALIDATORS, useExisting: MultiFieldValidatorDirective, multi: true }]
 })
 export class MultiFieldValidatorDirective implements Validator {
+  @Input() roundTrip: string[];
+
   validate(c: AbstractControl): ValidationErrors | null {
+    if (this.roundTrip?.length !== 2) {
+      return null;
+    }
+
     const group: FormGroup = c as FormGroup; // type cast
 
-    const fromCtrl = group.controls.from;
-    const toCtrl = group.controls.to;
+    const fromCtrl = group.controls[this.roundTrip[0]];
+    const toCtrl = group.controls[this.roundTrip[1]];
 
     if (!fromCtrl || !toCtrl) return null;
 
